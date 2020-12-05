1f48306c9c7bb094ce6346e7eacce3c16a2643c0
orloveste <orlovevm@yahoo.com>
2020-12-05 09:41:43 +0200

    Create java obg

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -1,7 +1,11 @@
 package main;
 
+import person.Person;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Bau");
+        Person person = new Person();
+        person.setName("ghita");
+        person.setId(1);
     }
 }
