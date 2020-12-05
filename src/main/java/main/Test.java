fe3978147f2bf028b48ce113e1a29e2034afd360
orloveste <orlovevm@yahoo.com>
2020-12-05 09:42:27 +0200

    deserializare

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import person.Person;
 
 public class Test {
@@ -7,5 +8,7 @@ public class Test {
         Person person = new Person();
         person.setName("ghita");
         person.setId(1);
+
+        ObjectMapper objectMapper = new ObjectMapper(); // deserializare
     }
 }
