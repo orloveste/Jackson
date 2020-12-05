74499e5909e632837042805ef9b6a89709486869
orloveste <orlovevm@yahoo.com>
2020-12-05 09:44:05 +0200

    arunci exceptie daca ere probleme

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import person.Person;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Person person = new Person();
         person.setName("ghita");
         person.setId(1);
 
         ObjectMapper objectMapper = new ObjectMapper(); // deserializare
+        objectMapper.writeValueAsString(person)
     }
 }
